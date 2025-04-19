16ca984f640831c97fb5353a6cfaed0daa9ab936
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-04-19 18:38:25 +0530

    feat: fixed ordering on xp

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -9,6 +9,6 @@ export const GET = withAuthentication(async (request: CustomRequest) => {
     const page = request.nextUrl.searchParams.get('page') || '1'
     const limit = request.nextUrl.searchParams.get('limit') || '10'
     const users = await userService.findAll({ _id: { $ne: request.user._id } }, parseInt(page), parseInt(limit), [], '-password')
-    const sortedUsers = users.data.sort((a, b) => a.xp - b.xp)
+    const sortedUsers = users.data.sort((a, b) => b.xp - a.xp)
     return NextResponse.json({ message: "success", users: sortedUsers }, { status: 200 })
 })
