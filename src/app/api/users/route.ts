de789befc8aa442ea472b8a15e5fa3fd3b6a65de
muhammedsirajudeen <muhammedsirajudeen29@gmail.com>
2025-04-22 00:22:36 +0530

    feat: added pagination and some more improvements as well

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -7,8 +7,9 @@ import { NextResponse } from "next/server"
 export const GET = withAuthentication(async (request: CustomRequest) => {
     const userService: UserService = container.resolve('UserService')
     const page = request.nextUrl.searchParams.get('page') || '1'
-    const limit = request.nextUrl.searchParams.get('limit') || '20'
+    const limit = request.nextUrl.searchParams.get('limit') || '9'
     const users = await userService.findAll({ _id: { $ne: request.user._id } }, parseInt(page), parseInt(limit), [], '-password')
+
     const sortedUsers = users.data.sort((a, b) => b.xp - a.xp)
-    return NextResponse.json({ message: "success", users: sortedUsers }, { status: 200 })
+    return NextResponse.json({ message: "success", users: sortedUsers, totalusers: users.total }, { status: 200 })
 })
--- src/utils/envConfig.ts
+++ src/utils/envConfig.ts
@@ -1,6 +1,6 @@
 export const envConfig = {
     MONGODB_URI: process.env.MONGODB_URI || 'mongodb://localhost:27017/nomads',
-    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SECRET: process.env.JWT_SECRET || '',
     GOOGLE_CLIENTID: process.env.NEXT_PUBLIC_GOOGLE_CLIENTID || '',
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || '1d',
 } as const 
